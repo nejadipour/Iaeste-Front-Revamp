6497c98f24df759a27a4798e110a57b7b62fbcca
mahsa <mahsa.shabaninezhad@gmail.com>
2023-12-06 05:33:15 +0330

    feat: add events constants

---
+++ src/constants/EventsConstants.js
@@ -0,0 +1,16 @@
+export const EVENT_FORMANT = {
+  inperson: "حضوری",
+  virtual: "آنلاین",
+  inperson_and_virtual: "حضوری و آنلاین",
+};
+
+export const EVENT_STATUS = {
+  attended: "attended",
+  register_open: 'register_open',
+  register_close: 'register_closed'
+};
+
+export const EVENT_TYPES = {
+  talk: 'talk',
+  workshop: 'workshop'
+}
