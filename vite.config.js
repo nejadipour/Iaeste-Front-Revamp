5221dc198313f96fd78b5127253dcf6fe2b8529c
mahsa <mahsa.shabaninezhad@gmail.com>
2023-12-06 05:30:35 +0330

    feat: add svgr

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from "vite-plugin-svgr";
+
 
 export default defineConfig(({mode, logLevel}) => {
 
     return {
-        plugins: [react()],
+        plugins: [react(), svgr({
+            // svgr options: https://react-svgr.com/docs/options/
+            svgrOptions: {
+              // ...
+            },
+          
+            // esbuild options, to transform jsx to js
+            esbuildOptions: {
+              // ...
+            },
+          
+            // A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include.
+            include: "**/*.svg?react",
+          
+            //  A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should ignore. By default no files are ignored.
+            exclude: "",
+          })],
         define: {},
     }
 })
