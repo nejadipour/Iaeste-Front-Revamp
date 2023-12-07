0a3f40e837592e46126df668de27d99d84d28089
a.nejadipour <nejadipour@gmail.com>
2023-12-07 23:51:31 +0330

    Add avatar and exam banner to profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,11 +1,14 @@
-import {App, Col, Row, Typography} from "antd";
+import {App, Avatar, Button, Card, Col, Row, Typography} from "antd";
 import {useCallback, useEffect, useState} from "react";
 import {useClient} from "../contexts/client/ClientContext.jsx";
 import axios from "axios";
 import EventCard from "../components/Event/EventCard.jsx";
+import {useAuth} from "../contexts/authentication/AuthContext.jsx";
+import {EditFilled} from "@ant-design/icons";
 
 export default function ProfilePage() {
     const {client} = useClient();
+    const {authUser} = useAuth();
     const [fetched, setFetched] = useState(false);
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState({
@@ -48,22 +51,52 @@ export default function ProfilePage() {
     }, [fetched, loading, fetchData])
 
     return (
-        <Row>
-            <Col md={6}>
+        <Row gutter={[24, 24]}>
+            <Col md={5}>
+                <div style={{textAlign: "center"}}>
+                    <Avatar
+                        size={120}
+                        src={authUser?.profile_pic}
+                    />
+                </div>
 
+                <div style={{textAlign: "center", marginTop: -32, marginRight: 60}}>
+                    <Button style={{
+                        zIndex: 1,
+                        padding: "8px 8px 12px 8px",
+                        backgroundColor: "rgba(255, 255, 255, 0.8)"
+                    }}><EditFilled style={{display: "flex"}}/></Button>
+                </div>
             </Col>
 
-            <Col md={18}>
+            <Col md={19}>
+                <Button
+                    type={"primary"}
+                    size={"large"}
+                    style={{
+                        width: "100%",
+                        height: 100,
+                        marginBottom: 24
+                    }}
+                >
+                    <Typography.Title level={2} style={{color: "white"}}>{"آزمون ۲۰۲۴"}</Typography.Title>
+                </Button>
+
                 <div style={{
                     borderRadius: 16,
                     boxShadow: "2px 2px 8px rgba(0, 0, 0, 0.26)",
                     padding: 24,
                 }}>
-                    <Typography.Title level={4}>{"رویداد‌های ثبت‌نامی"}</Typography.Title>
+                    <Typography.Title level={4}>{"رویداد‌های ثبت نامی"}</Typography.Title>
                     <div style={{overflow: "hidden"}}>
                         <div style={{display: "flex", flexWrap: "nowrap", whiteSpace: "nowrap", overflowX: "auto"}}>
                             {data.events.map((event) => (
-                                <div key={event.id} style={{minWidth: 400, width: 400, padding: "0px 9px 18px 9px", whiteSpace: "normal"}}>
+                                <div key={event.id} style={{
+                                    minWidth: 420,
+                                    width: 420,
+                                    padding: "0px 10px 20px 10px",
+                                    whiteSpace: "normal"
+                                }}>
                                     <EventCard key={event.id} data={event}/>
                                 </div>
                             ))}
