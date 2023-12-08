efc47394beea90b7b116029e962bc24e368d1780
a.nejadipour <nejadipour@gmail.com>
2023-12-08 20:17:01 +0330

    Add details and buttons of profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,10 +1,27 @@
-import {App, Avatar, Button, Card, Col, Row, Typography} from "antd";
+import {App, Avatar, Button, Col, ConfigProvider, Empty, Flex, Row, Typography} from "antd";
 import {useCallback, useEffect, useState} from "react";
 import {useClient} from "../contexts/client/ClientContext.jsx";
 import axios from "axios";
 import EventCard from "../components/Event/EventCard.jsx";
 import {useAuth} from "../contexts/authentication/AuthContext.jsx";
-import {EditFilled} from "@ant-design/icons";
+import {EditFilled, MailFilled, PhoneFilled, SyncOutlined} from "@ant-design/icons";
+import {useNavigate} from "react-router-dom";
+import FieldIcon from "../components/icons/FieldIcon.jsx";
+import UniversityIcon from "../components/icons/UniversityIcon.jsx";
+import UserIcon from "../assets/icons/user-fill.svg?react";
+import CooperationIcon from "../assets/icons/cooperation.svg?react";
+import UploadIcon from "../assets/icons/upload.svg?react";
+import {digitsEnToFa} from "@persian-tools/persian-tools";
+import GradientButton from "../components/Button/GradientButton.jsx";
+
+const cardStyle = {
+    borderRadius: 16,
+    boxShadow: "2px 2px 8px rgba(0, 0, 0, 0.26)",
+    padding: 24,
+    marginBottom: 24
+}
+
+const iconsStyle = {padding: "0px 0px 0px 5px"}
 
 export default function ProfilePage() {
     const {client} = useClient();
@@ -17,6 +34,7 @@ export default function ProfilePage() {
     });
     const eventsBaseQuery = "/events/";
     const {notification} = App.useApp();
+    const navigate = useNavigate();
 
     const fetchData = useCallback(() => {
         setLoading(true);
@@ -32,17 +50,18 @@ export default function ProfilePage() {
 
             setData({
                 ...data,
-                events: eventsResponse.data,
+                events: eventsResponse?.data?.filter((item) => item.userIsRegistered === true),
                 // jobs: jobsResponse.data,
             })
 
             setLoading(false);
         }))
             .catch(() => {
-                notification.error({message: "خطا در دریافت داده‌ها"})
+                notification.error({message: "خطا در دریافت داده‌ها"});
+                navigate("/");
             })
 
-    }, [client, data, notification])
+    }, [client, data, notification, navigate])
 
     useEffect(() => {
         if (!fetched && !loading) {
@@ -64,9 +83,54 @@ export default function ProfilePage() {
                     <Button style={{
                         zIndex: 1,
                         padding: "8px 8px 12px 8px",
-                        backgroundColor: "rgba(255, 255, 255, 0.8)"
+                        backgroundColor: "rgba(255, 255, 255, 0.8)",
+                        marginBottom: 24
                     }}><EditFilled style={{display: "flex"}}/></Button>
                 </div>
+
+                <div style={cardStyle}>
+                    <ConfigProvider
+                        theme={{
+                            token: {
+                                lineHeight: 0.5
+                            }
+                        }}
+                    >
+                        <Typography.Paragraph>
+                            <UserIcon style={iconsStyle}/>
+                            {`${authUser?.first_name || ""} ${authUser?.last_name || ""}`}
+                        </Typography.Paragraph>
+
+                        <Typography.Paragraph>
+                            <PhoneFilled rotate={90} style={iconsStyle}/>
+                            {authUser?.phone && digitsEnToFa(authUser.phone)}
+                        </Typography.Paragraph>
+
+                        <Typography.Paragraph>
+                            <MailFilled style={iconsStyle}/>
+                            {authUser?.email}
+                        </Typography.Paragraph>
+
+                        <Typography.Paragraph>
+                            <UniversityIcon style={iconsStyle}/>
+                            {authUser?.university}
+                        </Typography.Paragraph>
+
+                        <Typography.Paragraph>
+                            <FieldIcon style={iconsStyle}/>
+                            {authUser?.field}
+                        </Typography.Paragraph>
+                    </ConfigProvider>
+
+                    <Flex justify={"end"} gap={12} style={{marginTop: 50}}>
+                        <Button icon={<SyncOutlined />}>{"تغییر رمز عبور"}</Button>
+                        <GradientButton>{"ویرایش"}</GradientButton>
+                    </Flex>
+                </div>
+
+                <Button size={"large"} icon={<UploadIcon/>} style={{width: "100%", textAlign: "right", marginBottom: 12}}>{"بارگذاری رزومه"}</Button>
+                <Button size={"large"} icon={<CooperationIcon/>} style={{width: "100%", textAlign: "right", marginBottom: 24}}>{"درخواست همکاری"}</Button>
+
             </Col>
 
             <Col md={19}>
@@ -75,33 +139,38 @@ export default function ProfilePage() {
                     size={"large"}
                     style={{
                         width: "100%",
-                        height: 100,
+                        height: 120,
                         marginBottom: 24
                     }}
                 >
                     <Typography.Title level={2} style={{color: "white"}}>{"آزمون ۲۰۲۴"}</Typography.Title>
                 </Button>
 
-                <div style={{
-                    borderRadius: 16,
-                    boxShadow: "2px 2px 8px rgba(0, 0, 0, 0.26)",
-                    padding: 24,
-                }}>
+                <div style={cardStyle}>
                     <Typography.Title level={4}>{"رویداد‌های ثبت نامی"}</Typography.Title>
-                    <div style={{overflow: "hidden"}}>
-                        <div style={{display: "flex", flexWrap: "nowrap", whiteSpace: "nowrap", overflowX: "auto"}}>
-                            {data.events.map((event) => (
-                                <div key={event.id} style={{
-                                    minWidth: 420,
-                                    width: 420,
-                                    padding: "0px 10px 20px 10px",
-                                    whiteSpace: "normal"
-                                }}>
-                                    <EventCard key={event.id} data={event}/>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
+
+                    {data?.events?.length > 0 ?
+                        <div style={{overflow: "hidden"}}>
+                            <div style={{display: "flex", flexWrap: "nowrap", whiteSpace: "nowrap", overflowX: "auto"}}>
+                                {
+                                    data.events.map((event) => (
+                                        <div key={event.id} style={{
+                                            minWidth: 420,
+                                            width: 420,
+                                            padding: "0px 10px 20px 10px",
+                                            whiteSpace: "normal"
+                                        }}>
+                                            <EventCard key={event.id} data={event}/>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </div> :
+                        <Empty
+                            image={Empty.PRESENTED_IMAGE_SIMPLE}
+                            description={"شما در هیچ رویدادی ثبت نام نکرده‌اید."}
+                        />
+                    }
                 </div>
 
             </Col>
